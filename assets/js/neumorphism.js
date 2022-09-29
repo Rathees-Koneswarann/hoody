47776814f3479d2e47e25d9650888ba90aebc9af
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-09-29 13:46:44 +0530

    Fix the bug in scroll progress bar

--- assets/js/neumorphism.js
+++ assets/js/neumorphism.js
@@ -482,14 +482,20 @@ $(document).ready(function () {
 
     // change progress bar width with scrolling
     $(document).ready(function() {
-        var docHeight = $('#body').height(),
-        scrollPercent;
+        function getScrollPercent() {
+            var h = document.documentElement,
+                b = document.body,
+                st = 'scrollTop',
+                sh = 'scrollHeight';
+            return parseInt((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100) + "%";
+        }
       
         $(window).scroll(function() {
-          scrollPercent = ($(window).scrollTop() / docHeight) * 100;
-      
-          $('.scroll-progress').width(scrollPercent + '%');
+
+            var p = getScrollPercent();
+            $('.scroll-progress').css("width", p);
           });
           });
+
 
 });   
